ed6046c17226cf38e80b354bee39057aab069468
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-15 14:34:11 -0700

    WIP. rollup.config.js. remove biginteger and sha1

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-export * as sjcl from './sjcl/index';
-export { BigInteger } from './biginteger';
+export { BitArray } from './bitarray';
+export * as cipher from './cipher';
+export * as codec from './codec';
+export * as exception from './exception';
+export * as hash from './hash';
+export { random } from './prng';
--- src/sjcl/index.ts
+++
@@ -1,6 +0,0 @@
-export { BitArray } from './bitarray';
-export * as cipher from './cipher';
-export * as codec from './codec';
-export * as exception from './exception';
-export * as hash from './hash';
-export { random } from './prng';
